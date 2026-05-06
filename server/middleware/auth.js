d1262e70cdd7f44a402c109515e0286d860f23d9
whoami982 <185745027+whoami982@users.noreply.github.com>
2026-05-06 21:47:37 +0530

    Delete server directory

--- server/middleware/auth.js
+++
@@ -1,5 +0,0 @@
-function requireAuth(req, res, next) {
-  if (req.session && req.session.isAdmin) return next();
-  res.status(401).json({ error: 'Unauthorized' });
-}
-module.exports = { requireAuth };
